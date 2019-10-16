ce1aa13c28106854d3b1e9b4aabaa8044d3dd3ea
jlee627 <32377243+jlee627@users.noreply.github.com>
2019-10-15 17:16:28 -0700

    Update F-Capture.ps1

--- F-Capture.ps1
+++ F-Capture.ps1
@@ -377,7 +377,32 @@ function System-Info {}
 function Active-Processes { Get-Process | Out-File .\RunningProcesses.txt }
 function PhysicalMemory-Image {}
 function Disk-Image {}
-function Screenshot {}
+function Screenshot {
+param([Switch]$OfWindow)
+    begin {
+        Add-Type -AssemblyName System.Drawing
+        $jpegCodec = [Drawing.Imaging.ImageCodecInfo]::GetImageEncoders() | 
+            Where-Object { $_.FormatDescription -eq "JPEG" }
+    }
+    process {
+        Start-Sleep -Milliseconds 250
+        if ($OfWindow) {            
+            [Windows.Forms.Sendkeys]::SendWait("%{PrtSc}")        
+        } else {
+            [Windows.Forms.Sendkeys]::SendWait("{PrtSc}")        
+        }
+        Start-Sleep -Milliseconds 250
+        $bitmap = [Windows.Forms.Clipboard]::GetImage()    
+        $ep = New-Object Drawing.Imaging.EncoderParameters  
+        $ep.Param[0] = New-Object Drawing.Imaging.EncoderParameter ([System.Drawing.Imaging.Encoder]::Quality, [long]100)  
+        $screenCapturePathBase = "$pwd\ScreenCapture"
+        $c = 0
+        while (Test-Path "${screenCapturePathBase}${c}.jpg") {
+            $c++
+        }
+        $bitmap.Save("${screenCapturePathBase}${c}.jpg", $jpegCodec, $ep)
+    }
+}
 function Browser-Cookies {}
 function Browser-History {}
 function Peripheral-Devices {}
