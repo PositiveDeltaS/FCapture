cf905d219372dc29b9078ed0eb9e1d7bf1b1a45d
jlee627 <32377243+jlee627@users.noreply.github.com>
2019-10-18 23:13:58 -0700

    Update Screenshot.ps1

--- Scripts/Screenshot.ps1
+++ Scripts/Screenshot.ps1
@@ -1,3 +1,15 @@
+# The function of this script is to take
+# a screen shot of all windows. This includes
+# hidden and minimized windows, and the desktop
+# as well.
+
+#------------------------------------------------
+# Note: While currently the script can take
+# screenshots of current display, and desktop
+# well, it has trouble cycling through the 
+# accurate number of windows to alt+prt-sc.
+#------------------------------------------------
+
 function Screenshot {
   param([Switch]$OfWindow)
     
@@ -19,33 +31,37 @@ function Screenshot {
     
     Save-Screenshot
     
+    # Maximize all hidden processes
     (Get-Process | Where {$_.MainWindowTitle}).MainWindowTitle | % {
-      $winmax = (New-Object -ComObject wscript.shell)
-      $wshell.AppActivate($_)
+      $WShell = (New-Object -ComObject wscript.shell)
+      $WShell.AppActivate($_)
       sleep 1
-      $wshell.SendKeys("% x")
+      $WShell.SendKeys("% x")
       sleep 1
     } | Out-Null
     
+    # Skip FCapture Screenshot
     [Windows.Forms.Sendkeys]::Send("%{ESC}")
     start-sleep -Seconds 2
     
     # Cycle through active windows and screenshot them
     for($i = 0;$i -le $NumActiveWindow; $i++){
 
+      # Screenshot
       Start-Sleep -Milliseconds 250
       [Windows.Forms.Sendkeys]::SendWait("%{PrtSc}")
       Start-Sleep -Milliseconds 50
       #start-sleep -Seconds 2 
       Save-Screenshot
       
+      # Got to next window
       [Windows.Forms.Sendkeys]::Send("%{ESC}")
       start-sleep -Seconds 2
     } # End of for loop {}
     
     # Minimize all the Windows
-    $shell = New-Object -ComObject "Shell.Application"
-    $shell.MinimizeAll()
+    $Shell = New-Object -ComObject "Shell.Application"
+    $Shell.MinimizeAll()
     start-sleep -Seconds 2
     
     # Screencapture desktop once
@@ -55,15 +71,20 @@ function Screenshot {
     
     Save-Screenshot
     
-    # get your screen back
-    $shell.undominimizeall()
+    # Undo minimize
+    $Shell.UndoMinimizeAll()
   } # End of process {}
 } # End of Screenshot {}
 
 function Count-Windows{
+  # Count number of non-windows explorer windows
   $NumOtherWindows = (Get-Process | Where-Object {$_.MainWindowTitle -ne ""} | Select-Object MainWindowTitle).Count
+  
+  # Count number of window explorer windows
+  # NOTE: EXPRESSION BELOW CURRENTLY NOT WORKING AS INTENDED - Doesn't accurately give count 
   $NumExplorerWindows = (Get-Process | Where-Object {$_.MainWindowTitle -eq $_.HWND} | Select-Object MainWindowTitle).Count
   
+  # Sum up types of windows
   $WindowSum = $NumOtherWindows + $NumExplorerWindows
   Return $WindowSum
 }
