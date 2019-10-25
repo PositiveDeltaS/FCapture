ba21df351b03df06057aa2da028c7974df8c9ce2
Kelsey <troeger@pdx.edu>
2019-10-24 19:49:35 -0700

    amcache.ps1
    still some issues with amcache

--- Scripts/AMCache.ps1
+++ Scripts/AMCache.ps1
@@ -1,1 +1,32 @@
-function AmCache {}
+function AmCache {
+    
+    $path = "C:\Windows\appcompat\Programs\Amcache.hve"
+    $destination = "global:OUTPUT_DIR\"
+
+    
+    if (-not (Test-Path -Path $path)) {
+        throw "File not found: $path" 
+    }
+
+    $sourcefile = Split-Path -Path $path -Leaf
+    $destinationfile = Join-Path -Path $destination -ChildPath $sourcefile
+    $b4hash = Get-FileHash -Path $path
+    
+    try {
+        Copy-Item -Path $path -Destination $destination -Force -ErrorAction Stop
+    }
+    catch {
+        throw "Copying Amcache.hve failed"
+    }
+    finally {
+        $afhash = Get-FileHash -Path $destinationfile
+        if ($afhash.Hash -ne $b4hash.Hash) {
+            throw "Amcache.hve corrupted during copy"
+        }
+        else {
+            Write-Information -MessageData "Amcache.hve copied successfully" -InformationAction Continue
+        }
+    }
+ }
+
+ Amcache
