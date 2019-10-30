ea63096cc7912a1f67d381604d90e51f40e57908
jlee627 <32377243+jlee627@users.noreply.github.com>
2019-10-29 21:55:56 -0700

    Update Scheduled-Tasks.ps1

--- Data/Scripts/Scheduled-Tasks.ps1
+++ Data/Scripts/Scheduled-Tasks.ps1
@@ -1,1 +1,7 @@
-function Scheduled-Tasks {}
+function Scheduled-Tasks {
+  if(Test-Path -Path "$global:OUTPUT_DIR\Tasks"){
+    Copy-Item C:\Windows\System32\Tasks -Filter *.xml -Destination $global:OUTPUT_DIR\Tasks -Recurse
+  } else{
+    New-Item -ItemType directory -Path $global:OUTPUT_DIR\Tasks
+  }
+}
