9b25edd77c88385b8b5aa79cf3b37b09d22241e0
jlee627 <32377243+jlee627@users.noreply.github.com>
2019-10-29 22:48:27 -0700

    Update Scheduled-Tasks.ps1

--- Data/Scripts/Scheduled-Tasks.ps1
+++ Data/Scripts/Scheduled-Tasks.ps1
@@ -1,7 +1,12 @@
 function Scheduled-Tasks {
-  if(Test-Path -Path "$global:OUTPUT_DIR\Tasks"){
-    Copy-Item C:\Windows\System32\Tasks -Filter *.xml -Destination $global:OUTPUT_DIR\Tasks -Recurse
-  } else{
-    New-Item -ItemType directory -Path $global:OUTPUT_DIR\Tasks
-  }
+  #Copy all directories in \Task and copy all files with extension .xml 
+  Copy-Item C:\Windows\System32\Tasks -Filter *.xml -Destination $global:OUTPUT_DIR\Tasks -Recurse
+  
+  #Clean up all empty directories that were copied
+  $clean="$global:OUTPUT_DIR\Tasks"
+  
+  do {
+    $dirs = gci $clean -directory -recurse | Where { (gci $_.fullName).count -eq 0 } | select -expandproperty FullName
+    $dirs | Foreach-Object { Remove-Item $_ }
+  } while ($dirs.count -gt 0)
 }
