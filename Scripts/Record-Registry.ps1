f9d0bbcdc52b9df10c48f5fb270e49123b956317
Kelsey <troeger@pdx.edu>
2019-10-31 18:08:55 -0700

    record-registry function
    record registry function added here instead of my branch (sorry)

--- Scripts/Record-Registry.ps1
+++ Scripts/Record-Registry.ps1
@@ -1,1 +1,9 @@
-function Record-Registry {}
+function Record-Registry {
+
+    reg export “HKEY_CLASSES_ROOT” c:\windows\temp\sh
+    reg export “HKEY_CURRENT_USER” c:\windows\temp\shim.reg
+    reg export “HKEY_LOCAL_MACHINE” c:\windows\temp\shim.reg
+    reg export “HKEY_USERS” c:\windows\temp\shim.reg
+    reg export “HKEY_CURRENT_CONFIG” c:\windows\temp\shim.reg
+
+    }
