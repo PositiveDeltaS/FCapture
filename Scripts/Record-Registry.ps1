645406586cf94fa1d59e87817c39dfbfab4489db
Kelsey <troeger@pdx.edu>
2019-10-31 18:28:00 -0700

    Record-registry complete
    Record-registry checks for a Registry folder in the output directory and creates one, then exports entire registry into .reg files

--- Scripts/Record-Registry.ps1
+++ Scripts/Record-Registry.ps1
@@ -1,9 +1,15 @@
 function Record-Registry {
+    
+     $path = "$global:OUTPUT_DIR\Registry"
 
-    reg export “HKEY_CLASSES_ROOT” c:\windows\temp\sh
-    reg export “HKEY_CURRENT_USER” c:\windows\temp\shim.reg
-    reg export “HKEY_LOCAL_MACHINE” c:\windows\temp\shim.reg
-    reg export “HKEY_USERS” c:\windows\temp\shim.reg
-    reg export “HKEY_CURRENT_CONFIG” c:\windows\temp\shim.reg
+     if (-not (Test-Path -Path $path)) {
+        New-Item -ItemType directory -Path $global:OUTPUT_DIR\Registry
+    }
+
+    reg export “HKEY_CLASSES_ROOT” $global:OUTPUT_DIR\Registry\Hkey_classes_root.reg
+    reg export “HKEY_CURRENT_USER” $global:OUTPUT_DIR\Registry\Hkey_current_user.reg
+    reg export “HKEY_LOCAL_MACHINE” $global:OUTPUT_DIR\Registry\Hkey_local_machine.reg
+    reg export “HKEY_USERS” $global:OUTPUT_DIR\Registry\Hkey_users.reg
+    reg export “HKEY_CURRENT_CONFIG” $global:OUTPUT_DIR\Registry\Hkey_current_config.reg
 
     }
