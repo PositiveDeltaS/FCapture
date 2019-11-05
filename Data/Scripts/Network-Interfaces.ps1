2a09b026957d96429b09a7306a4c623b0e403765
nfrie2 <nfrie2@pdx.edu>
2019-11-04 16:58:55 -0800

    Fixed missing try for logging

--- Data/Scripts/Network-Interfaces.ps1
+++ Data/Scripts/Network-Interfaces.ps1
@@ -1,11 +1,13 @@
 ﻿function Network-Interfaces {
 	$File = "$global:OUTPUT_DIR\NetworkInterfaces.txt"
 
-	write-output "Network Interface Summary" | Out-File $File
-	write-output "================================================================" | Out-File $File -Append
-	Get-NetAdapter -Name "*" -IncludeHidden | Format-Table -Property "Name", "Status", "InterfaceDescription", "InterfaceName" | Out-File $File -Append 
-	ipconfig /all | Out-File $File -Append
-	write-output "`n`n" | Out-File $File -Append
+	try
+	{	
+		write-output "Network Interface Summary" | Out-File $File
+		write-output "================================================================" | Out-File $File -Append
+		Get-NetAdapter -Name "*" -IncludeHidden | Format-Table -Property "Name", "Status", "InterfaceDescription", "InterfaceName" | Out-File $File -Append 
+		ipconfig /all | Out-File $File -Append
+		write-output "`n`n" | Out-File $File -Append
 
 		write-output "Network Interface Detailed Properties" | Out-File $File -Append
 		write-output "================================================================" | Out-File $File -Append
