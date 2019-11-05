b502ad6b41097f3123611e9c905e3342d5ff8952
nfrie2 <nfrie2@pdx.edu>
2019-11-04 16:49:37 -0800

    Added logging

--- Data/Scripts/Network-Interfaces.ps1
+++ Data/Scripts/Network-Interfaces.ps1
@@ -7,11 +7,18 @@
 	ipconfig /all | Out-File $File -Append
 	write-output "`n`n" | Out-File $File -Append
 
-	write-output "Network Interface Detailed Properties" | Out-File $File -Append
-	write-output "================================================================" | Out-File $File -Append
-	Get-NetAdapter -Name "*" -IncludeHidden | ForEach-Object {
-		write-output "--------------------------------" | Out-File $File -Append -NoNewLine
-		write-output($_.Name)  | Out-File $File -Append
-		Write-output ($_ | Format-List -Property "*" | Out-String)  | Out-File $File -Append	
+		write-output "Network Interface Detailed Properties" | Out-File $File -Append
+		write-output "================================================================" | Out-File $File -Append
+		Get-NetAdapter -Name "*" -IncludeHidden | ForEach-Object {
+			write-output "--------------------------------" | Out-File $File -Append -NoNewLine
+			write-output($_.Name)  | Out-File $File -Append
+			Write-output ($_ | Format-List -Property "*" | Out-String)  | Out-File $File -Append	
+		}
+		Search-And-Add-Log-Entry $SUCCESS_LOG "Network-Interfaces"
+	}
+	catch
+	{
+		if(Test-Path -Path $File){rm $File}
+		Search-And-Add-Log-Entry $SUCCESS_LOG "Network-Interfaces"
 	}
 }
