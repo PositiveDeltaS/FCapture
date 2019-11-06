76dde56bf4f3b7666e4e6bde6a1fe789107180e0
Kelsey <troeger@pdx.edu>
2019-11-05 18:08:09 -0800

    Amcache registry
    This script takes the amcache registry entry and copies it to the global output directory. It doesn't touch amcache.hve

--- Scripts/AMCache.ps1
+++ Scripts/AMCache.ps1
@@ -1,32 +1,11 @@
 function AmCache {
     
-    $path = "C:\Windows\appcompat\Programs\Amcache.hve"
-    $destination = "global:OUTPUT_DIR\"
+   $path = "$global:OUTPUT_DIR"
 
-    
-    if (-not (Test-Path -Path $path)) {
-        throw "File not found: $path" 
-    }
-
-    $sourcefile = Split-Path -Path $path -Leaf
-    $destinationfile = Join-Path -Path $destination -ChildPath $sourcefile
-    $b4hash = Get-FileHash -Path $path
-    
-    try {
-        Copy-Item -Path $path -Destination $destination -Force -ErrorAction Stop
-    }
-    catch {
-        throw "Copying Amcache.hve failed"
-    }
-    finally {
-        $afhash = Get-FileHash -Path $destinationfile
-        if ($afhash.Hash -ne $b4hash.Hash) {
-            throw "Amcache.hve corrupted during copy"
-        }
-        else {
-            Write-Information -MessageData "Amcache.hve copied successfully" -InformationAction Continue
-        }
-    }
+     if (-not (Test-Path -Path $path)) {
+        throw "Output directory doesn't exist"
+     }
+     else {
+      reg export “HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\AppCompatCache” $path\Amcache.reg
+     }
  }
-
- Amcache
