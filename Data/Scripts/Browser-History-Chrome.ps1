1c1f9c536ab7fff67135d6c5259ca056dedaf8f4
Jordan Kremer <jkrem@pdx.edu>
2019-11-07 17:36:31 -0800

    Fix sub directory pathing, condensed directories to file

--- Data/Scripts/Browser-History-Chrome.ps1
+++ Data/Scripts/Browser-History-Chrome.ps1
@@ -1,24 +1,30 @@
-function Browser-History-Chrome($path) {
+function Browser-History-Chrome {
 
-$historyPathList = Resolve-Path "$env:HOMEDRIVE\Users\*\AppData\Local\Google\Chrome\User Data\Default\History" | Select -ExpandProperty Path
+	#get all users 
+	$userPathList = Resolve-Path "$env:HOMEDRIVE\Users\*" | Select -ExpandProperty Path
 
+	Write-Host $userPathList
 
-ForEach($historyPath in $historyPathList)
-    {
-        # Make a path to save output
-        # Maintain/copy hierarchy because it's simple and straight-forward
-        $unqualifiedPath = Split-Path -Path (Split-Path -Path $historyPath -NoQualifier) -Parent
-        $completeOutPath   = "$global:OUTPUT_DIR\History\Chrome$unqualifiedPath"
-
-        try
-        { 
-            New-Item -Path $completeOutPath -ItemType Directory
-            Copy-Item $historyPath -Destination $completeOutPath -Recurse
-            Search-And-Add-Log-Entry $SUCCESS_LOG "Copied chrome history directories/files at $historyPath"
-        }
-        catch
-        { 
-            Search-And-Add-Log-Entry $FAIL_LOG "Failed to copy chrome history directories/files at $historyPath"
-        }
-    }
+	ForEach($userPath in $userPathList)
+	{
+		$unqualifiedPath = Split-Path -Path $userPath -NoQualifier
+		$completeOutPath   = "$global:OUTPUT_DIR\BrowserData$unqualifiedPath\Chrome"
+		$historyFilePath = $userPath + "\AppData\Local\Google\Chrome\User Data\Default\History"
+		
+		<#
+		Write-Host "UnqualifiedPath: " $unqualifiedPath
+		Write-Host "HistoryFilePath: " $historyPath
+		Write-Host "CompleteOutPath: " $completeOutPath
+		#>
+		try
+		{ 
+			New-Item -Path $completeOutPath -ItemType Directory
+			Copy-Item $historyFilePath -Destination $completeOutPath -Recurse
+			Search-And-Add-Log-Entry $SUCCESS_LOG "Copied files at $historyPath"
+		}
+		catch
+		{ 
+			Search-And-Add-Log-Entry $FAIL_LOG "Failed to copy files at $historyPath"
+		}
+	}
 }
