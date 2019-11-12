a35c4b36a15557b32f67929085cfee0c1615bef6
JordanKremer <jordan.kremer@gmail.com>
2019-11-11 21:10:27 -0800

    Update Browser-History.ps1

--- Data/Scripts/Browser-History.ps1
+++ Data/Scripts/Browser-History.ps1
@@ -1,18 +1,7 @@
 ##Wrapper to get History and cache
 function Browser-History {
 
-	Browser-History-Chrome
-	#Browser-Cache-Chrome
-	
-	
-	Browser-History-Edge
-	<#Browser-Edge-Cache
-	
-	Browser-Firefox-History
-	Browser-Firefox-Cache
-	
-	
-	#>
+Browser-Data-Retrieval
 	
 
 }
