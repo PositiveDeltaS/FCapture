8faf6e75bc1c7430f4595300c5b1bc1bfc7c3772
Alec Deschene <alecdeschene@users.noreply.github.com>
2019-11-13 03:18:47 -0800

    Update Network-Share, Remote-Desktop, and Shellbags
    - Added functionality for Shellbags, RDPCache, and Network Share Info scripts.
    - They may not be getting all of the desired information, but they're at least a good start
    - More info on these will need to be gathered during the meeting with sponsors
    - Also added a small check to Jump-Lists

--- Data/Scripts/Jump-List.ps1
+++ Data/Scripts/Jump-List.ps1
@@ -3,6 +3,9 @@ function Jump-List
     # Get a list of names of users that actually have jump lists
     $users       = Split-Path -Path (Resolve-Path "$env:HOMEDRIVE\Users\*" | Select -ExpandProperty Path) -Leaf
     $usefulUsers = $users | Where {Test-Path "$env:HOMEDRIVE\Users\$_\AppData\Roaming\Microsoft\Windows\Recent"}
+    
+    # If there are no Jump List files in any user account, just log as much and abort
+    if($usefulUsers.Length -lt 1) { Search-And-Add-Log-Entry $SUCCESS_LOG "No jump files in any user accounts; skipped"; return }
 
     # Copy the jump lists of each user to the output directory
     ForEach($user in $usefulUsers)
