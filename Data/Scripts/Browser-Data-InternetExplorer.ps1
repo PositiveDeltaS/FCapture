87a7b7222986ff8343ac6c9a602e38a69ab118d4
Jordan Kremer <jkrem@pdx.edu>
2019-11-14 19:04:40 -0800

    Remove Browser-Data-InternetExplorer/Edge/Opera/FireFox

--- Data/Scripts/Browser-Data-InternetExplorer.ps1
+++
@@ -1,9 +0,0 @@
-function Browser-Data-InternetExplorer
-{
-	
-	$path = "AppData\Local\Microsoft\Internet Explorer"
-	$outPath = "Internet Explorer\Data"
-	$users = Get-Desired-User-Profile-Names $path
-	Get-Browser-Data $users $path $outPath
-
-}
