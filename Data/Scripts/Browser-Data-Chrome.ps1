0c068e9c0eeef420eb0a4ed68c81307e7d24bccf
Jordan Kremer <jkrem@pdx.edu>
2019-11-14 19:01:35 -0800

    Remove browser-data-chrome

--- Data/Scripts/Browser-Data-Chrome.ps1
+++
@@ -1,7 +0,0 @@
-function Browser-Data-Chrome 
-{
-	$path = "AppData\Local\Google\Chrome\User Data\Default"
-	$outPath = "Chrome\Data"
-	$users = Get-Desired-User-Profile-Names $path
-	Get-Browser-Data $users $path $outPath
-}
