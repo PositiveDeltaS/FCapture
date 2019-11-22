e25ca31c198a886ba2e5f384b433754b443743c1
Kelsey <troeger@pdx.edu>
2019-11-21 21:14:43 -0800

    systeminformation
    added error checking to system information

--- Data/Scripts/System-Info.ps1
+++ Data/Scripts/System-Info.ps1
@@ -1,1 +1,14 @@
-function System-Info { Get-ComputerInfo | Out-File "$global:OUTPUT_DIR\SystemInfo.txt" }
+function System-Info { 
+    Get-ComputerInfo | Out-File "$global:OUTPUT_DIR\SystemInfo.txt"
+    $outputFilename = "SystemInfo.txt" 
+    if(Test-Path "$global:OUTPUT_DIR\$outputFilename.*")
+    {
+        Search-And-Add-Log-Entry $SUCCESS_LOG ("Created $outputFilename output file successfully")
+        return $true
+    }
+    else
+    {
+        Search-And-Add-Log-Entry $FAIL_LOG ("Failed to create $outputFilename output file")
+        return $false
+    }
+}
