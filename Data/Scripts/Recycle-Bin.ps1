99d87732900ac4003c6a831a9cb7ea7412fd1001
Kelsey <troeger@pdx.edu>
2019-11-26 13:31:51 -0800

    update recycle bin to match master
    git is being annoying

--- Data/Scripts/Recycle-Bin.ps1
+++ Data/Scripts/Recycle-Bin.ps1
@@ -1,6 +1,4 @@
-
 function Get-Recycle-Bin {
-
 #  ---Temporary function---
 
     # This is a temporary function for use until the original function is bugfixed
@@ -9,23 +7,15 @@ function Get-Recycle-Bin {
 
     Copy-Item -Path $RBPath -Destination $outPath -Recurse -Force -ErrorAction SilentlyContinue
 
-    return true
-}
-
 <# ---Original function---
->>>>>>> Stashed changes
-
  [CmdletBinding()]
     param ( [string]$path, [string]$destination)
-
  if (-not (Test-Path -Path $path)) {
    throw "File not found: $path" 
    }
-
  $sourcefile = Split-Path -Path $path -Leaf
  $destinationfile = Join-Path -Path $destination -ChildPath $sourcefile
  $b4hash = Get-FileHash -Path $path
-
  try {
     Copy-Item -Path $path -Destination $destination -Recurse -Force -ErrorAction Stop
  }
@@ -41,7 +31,7 @@ function Get-Recycle-Bin {
      Write-Information -MessageData "File copied successfully" -InformationAction Continue
    }
  }
-} 
+ #>
+}
 # This line is outside of the function so it's being called immediately upon starting the program and causing errors
-#Get-Recycle-Bin 'C:\$Recycle.Bin\*' "$global:OUTPUT_DIR\RecycleBinFiles" | Out-File "$global:OUTPUT_DIR\Recyclebincontents.txt"
-#>
+# Get-Recycle-Bin 'C:\$Recycle.Bin\*' "$global:OUTPUT_DIR\RecycleBinFiles" | Out-File "$global:OUTPUT_DIR\RecycleBinContents.txt"
