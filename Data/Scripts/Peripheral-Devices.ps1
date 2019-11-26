f3a9865d6519a43d6ff3b9cdb1438a548b34855f
Jordan Kremer <jkrem@pdx.edu>
2019-11-25 20:55:31 -0800

    Add success/failure to peripheral devices

--- Data/Scripts/Peripheral-Devices.ps1
+++ Data/Scripts/Peripheral-Devices.ps1
@@ -1,1 +1,14 @@
-function Peripheral-Devices { Get-PnPDevice | Export-Csv -Path "$global:OUTPUT_DIR\peripheral_devices.csv" }
+function Peripheral-Devices { 
+	Get-PnPDevice | Export-Csv -Path "$global:OUTPUT_DIR\peripheral_devices.csv" 
+
+    if(Test-Path "$global:OUTPUT_DIR\peripheral_devices.csv")
+    {
+        Search-And-Add-Log-Entry $SUCCESS_LOG ("Created peripheral_devices.csv output file successfully")
+        return $true
+    }
+    else
+    {
+        Search-And-Add-Log-Entry $FAIL_LOG ("Failed to create peripheral_devices.csv output file")
+        return $false
+    }
+}
