e10c26d4664bbf69e686177966c9674fd73b2518
Kelsey <troeger@pdx.edu>
2019-11-26 15:49:08 -0800

    installed programs error checking
    added error checking to installed-programs.ps1

--- Data/Scripts/Installed-Programs.ps1
+++ Data/Scripts/Installed-Programs.ps1
@@ -1,1 +1,16 @@
-function Installed-Programs {  Get-ItemProperty HKLM:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Format-Table -AutoSize }
+function Installed-Programs {  
+
+    Get-ItemProperty HKLM:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Format-Table -AutoSize | Out-File "$global:OUTPUT_DIR\InstalledPrograms.txt"
+
+    $outputFilename = "InstalledPrograms.txt"
+    if(Test-Path "$global:OUTPUT_DIR\InstalledPrograms.txt")
+    {
+        Search-And-Add-Log-Entry $SUCCESS_LOG ("Created $outputFilename output file successfully")
+        return $true
+    }
+    else
+    {
+        Search-And-Add-Log-Entry $FAIL_LOG ("Failed to create $outputFilename output file")
+        return $false
+    }
+}
