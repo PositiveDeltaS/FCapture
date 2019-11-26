64fa7e5ea1ab7a26349e1f2a824208d516fc1a08
Kelsey <troeger@pdx.edu>
2019-11-26 15:25:49 -0800

    record-registry error check
    added error checking methods to record-registry file. script sets a flag to false if any of the individual registry files fails to output and returns false if the flag is set. this ensures that the function still attempts to export all the registry files even if one fails early.

--- Data/Scripts/Record-Registry.ps1
+++ Data/Scripts/Record-Registry.ps1
@@ -6,10 +6,40 @@ function Record-Registry {
         New-Item -ItemType directory -Path $global:OUTPUT_DIR\Registry
     }
 
-    reg export “HKEY_CLASSES_ROOT” $global:OUTPUT_DIR\Registry\Hkey_classes_root.reg
-    reg export “HKEY_CURRENT_USER” $global:OUTPUT_DIR\Registry\Hkey_current_user.reg
-    reg export “HKEY_LOCAL_MACHINE” $global:OUTPUT_DIR\Registry\Hkey_local_machine.reg
-    reg export “HKEY_USERS” $global:OUTPUT_DIR\Registry\Hkey_users.reg
-    reg export “HKEY_CURRENT_CONFIG” $global:OUTPUT_DIR\Registry\Hkey_current_config.reg
+    $flag = $true
 
+    reg export “HKEY_CLASSES_ROOT” $global:OUTPUT_DIR\Registry\Hkey_classes_root.reg
+    if(-not (Test-Path "$global:OUTPUT_DIR\Registry\Hkey_classes_root.reg")) {
+        $flag = $false
     }
+
+    reg export “HKEY_CURRENT_USER” $global:OUTPUT_DIR\Registry\Hkey_current_user.reg
+    if(-not (Test-Path "$global:OUTPUT_DIR\Registry\Hkey_current_user.reg")) {
+         $flag = $false
+    }
+
+    reg export “HKEY_LOCAL_MACHINE” $global:OUTPUT_DIR\Registry\Hkey_local_machine.reg
+    if(-not (Test-Path "$global:OUTPUT_DIR\Registry\Hkey_local_machine.reg")) {
+         $flag = $false
+    }
+
+    reg export “HKEY_USERS” $global:OUTPUT_DIR\Registry\Hkey_users.reg
+    if(-not (Test-Path "$global:OUTPUT_DIR\Registry\Hkey_users.reg")) {
+         $flag = $false
+    }
+
+    reg export “HKEY_CURRENT_CONFIG” $global:OUTPUT_DIR\Registry\Hkey_current_config.reg
+    if(-not (Test-Path "$global:OUTPUT_DIR\Registry\Hkey_current_config.reg")) {
+         $flag = $false
+    }
+
+    if($flag -eq $false) {
+        Search-And-Add-Log-Entry $FAIL_LOG ("Failed to create output file for one or more registry files in Record-Registry function")
+        return $false
+    }
+    else {
+        Search-And-Add-Log-Entry $SUCCESS_LOG ("Created Registry output files successfully")
+        return $true
+    }
+    
+  }
