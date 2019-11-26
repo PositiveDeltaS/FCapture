3334774c35402f6d8c30cf55c56f1744716e518c
Kelsey <troeger@pdx.edu>
2019-11-26 15:40:48 -0800

    user accounts list error checking
    added error checking to user-accounts.ps1

--- Data/Scripts/User-Accounts.ps1
+++ Data/Scripts/User-Accounts.ps1
@@ -1,1 +1,16 @@
-function User-Accounts { Get-LocalUser | Select * | Out-File "$global:OUTPUT_DIR\Userlist.txt" }
+function User-Accounts { 
+
+    Get-LocalUser | Select * | Out-File "$global:OUTPUT_DIR\Userlist.txt" 
+    
+    $outputFilename = "Userlist.txt"
+    if(Test-Path "$global:OUTPUT_DIR\Userlist.txt")
+    {
+        Search-And-Add-Log-Entry $SUCCESS_LOG ("Created $outputFilename output file successfully")
+        return $true
+    }
+    else
+    {
+        Search-And-Add-Log-Entry $FAIL_LOG ("Failed to create $outputFilename output file")
+        return $false
+    }
+ }
