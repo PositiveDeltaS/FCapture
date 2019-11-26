1b9c987c90875535a7775952b1ec6487306ce5de
Kelsey <troeger@pdx.edu>
2019-11-26 15:34:16 -0800

    startup items error checking
    added error checking to startup-items

--- Data/Scripts/Startup-Items.ps1
+++ Data/Scripts/Startup-Items.ps1
@@ -1,1 +1,18 @@
-function Startup-Programs {  Get-CimInstance Win32_StartupCommand | Select-Object Name, Command, Location, User | Format-List | Out-File "$global:OUTPUT_DIR\StartupPrograms.txt" }
+function Startup-Programs {  
+
+    Get-CimInstance Win32_StartupCommand | Select-Object Name, Command, Location, User | Format-List | Out-File "$global:OUTPUT_DIR\StartupPrograms.txt" 
+    
+    $outputFilename = "StartupPrograms.txt"
+    if(Test-Path "$global:OUTPUT_DIR\StartupPrograms.txt")
+    {
+        Search-And-Add-Log-Entry $SUCCESS_LOG ("Created $outputFilename output file successfully")
+        return $true
+    }
+    else
+    {
+        Search-And-Add-Log-Entry $FAIL_LOG ("Failed to create $outputFilename output file")
+        return $false
+    }
+ }
+
+    
