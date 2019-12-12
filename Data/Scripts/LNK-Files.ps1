16d4538d91297d66fc79930687ba826e05a5d43d
Petar Crljenica <petar.crljenica@gmail.com>
2019-12-12 14:25:36 -0800

    Add files via upload

--- Data/Scripts/LNK-Files.ps1
+++ Data/Scripts/LNK-Files.ps1
@@ -1,1 +1,23 @@
-function LNK {}
+function LNK {
+     Try{
+    #Copy all directories in \Task and copy all files with extension .xml 
+    Copy-Item C:\ProgramData\Microsoft\Windows\Start Menu\Programs -Destination $global:OUTPUT_DIR\ -Recurse
+  
+    #Clean up all empty directories that were copied
+    $clean="$global:OUTPUT_DIR\LNK"
+  
+    do {
+      $dirs = gci $clean -directory -recurse | Where { (gci $_.fullName).count -eq 0 } | select -expandproperty FullName
+      $dirs | Foreach-Object { Remove-Item $_ }
+    } while ($dirs.count -gt 0)
+    
+    # Log success
+    Search-And-Add-Log-Entry $SUCCESS_LOG ("Created $OUTPUT_DIR\LNK directory successfully")
+        return $true
+  } # End of try block
+  Catch {
+    # Log failure 
+    Search-And-Add-Log-Entry $FAIL_LOG ("Failed to create $OUTPUT_DIR\LNK directory")
+        return $false
+  } # End of catch block
+}
