47bf116d658cc2d960e36ab86a787ee87054e7e1
Petar Crljenica <petar.crljenica@gmail.com>
2019-12-12 13:57:21 -0800

    Add files via upload

--- Data/Scripts/Event-Logs.ps1
+++ Data/Scripts/Event-Logs.ps1
@@ -1,1 +1,15 @@
-function Event-Logs {}
+function Event-Logs {
+
+    Get-WinEvent -ListLog * | Out-File "$global:OUTPUT_DIR\EventLogs.txt"
+    $outputFilename = "EventLogs.txt"
+    if(Test-Path "$global:OUTPUT_DIR\EventLogs.txt")
+    {
+        Search-And-Add-Log-Entry $SUCCESS_LOG ("Created $outputFilename output file successfully")
+        return $true
+    }
+    else
+    {
+        Search-And-Add-Log-Entry $FAIL_LOG ("Failed to create $outputFilename output file")
+        return $false
+    }
+}
--- Data/Scripts/ShimCache.ps1
+++ Data/Scripts/ShimCache.ps1
@@ -1,1 +1,19 @@
-function ShimCache {}
+function ShimCache {
+    # Set up path key of registry entry and path to output directory
+    $key = "HKLM\SYSTEM\CurrentControlSet\Control\SessionManager\AppCompatCache\AppCompatCache"
+    $path = "$global:OUTPUT_DIR\ShimCache.reg"
+
+    # Save the registry info contained in $key to a new .reg file and save it at $path
+    reg export $key $path /y
+
+    if (Test-Path $path) # Validate the created path to determine success; Log results
+    {
+        Search-And-Add-Log-Entry $SUCCESS_LOG ("Created ShimCache.reg")
+        return $true;
+    }
+    else
+    {
+        Search-And-Add-Log-Entry $FAIL_LOG ("Failed to create ShimCache.reg")
+        return $false;
+    }
+}
